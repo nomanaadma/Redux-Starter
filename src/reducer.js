c4f164385fd1ddb38fd75b30ebac4e25360232cf
Noman Shoukat <nomanaadma@gmail.com>
2020-09-10 07:16:29 -0700

    Reducer

---
+++ src/reducer.js
@@ -0,0 +1,21 @@
+let lastID = 0;
+
+function reducer(state = [], action) {
+
+    switch (action.type) {
+        case "bugAdd":
+            return [
+                ...state,
+                {
+                    id: ++lastID,
+                    description: action.payload.description,
+                    resolved: false
+                }
+            ];
+        case "bugRemove":
+            return state.filter(bug => bug.id !== action.payload.id);
+        default:
+            return state;
+    }
+    
+}
