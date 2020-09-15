1e1b8dc7aee283f2987bd89b6c441844734a5b65
Noman Shoukat <nomanaadma@gmail.com>
2020-09-15 17:46:21 +0500

    Refactor Code Using Slice

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,29 +1,27 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-
-// ACTION CREATORS SECTION
-export const bugAdd = createAction("bugAdd");
-export const bugResolve = createAction("bugResolve");
-
-
-
-// REDUCER SECTION 
 let lastID = 0;
 
+const slice = createSlice({
+    name: 'bugs',
+    initialState: [],
+    reducers: {
+        
+        bugAdd: (bugs, action) => {
+            bugs.push({
+                id: ++lastID,
+                description: action.payload.description,
+                resolved: false
+            });
+        },
+        
+        bugResolve: (bugs, action) => {
+            const index = bugs.findIndex(bug => bug.id === action.payload.id );
+            bugs[index].resolved = true;
+        }
 
-export default createReducer([], {
-   
-    [bugAdd.type]: (bugs, action) => {
-        bugs.push({
-            id: ++lastID,
-            description: action.payload.description,
-            resolved: false
-        });
-    },
-
-    [bugResolve.type]: (bugs, action) => {
-        const index = bugs.findIndex(bug => bug.id === action.payload.id );
-        bugs[index].resolved = true;
     }
-
 });
+
+export const { bugAdd, bugResolve} = slice.actions;
+export default slice.reducer;
