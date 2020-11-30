7dd3d85d14c8f525518fd94fddaff2f13b192c1c
Noman Shoukat <nomanaadma@gmail.com>
2020-11-30 23:39:34 +0500

    Toastify Exercise

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import configureStore from './store/configureStore';
 
 const store = configureStore();
 
-store.dispatch((dispatch, getState) => {
-	
-	dispatch({ type: 'bugsReceived', bugs: [1,2,3] });
-
+store.dispatch({
+	type: 'error',
+	payload: { message: "An Error Occured" }
 });
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import reducer from './reducers';
 import logger from './middlewares/logger';
-import func from './middlewares/func';
+import toast from './middlewares/toast';
 
 export default function() {
     return configureStore({ 
         reducer,
-        middleware: [logger('console'), func]
+        middleware: [...getDefaultMiddleware(), toast, logger('console'), toast]
     });
 };
