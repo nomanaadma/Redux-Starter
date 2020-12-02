5164c85cbd5d2645bb8bb9684b75e37595f419ab
Noman Shoukat <nomanaadma@gmail.com>
2020-12-02 19:02:50 +0500

    Fake Api using Mock Adapter

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,14 +1,21 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import { addbugs } from '../bugs';
 import configureStore from '../configureStore';
 
 describe('Bug Slice', () => {
 	
 	it("Should Handle the addBug Action", async () => {
+		
+		const bug = { description: 'a'};
+		const saveBug = { ...bug, id: 1 };
+
+		const fakeAxios = new MockAdapter(axios);
+		fakeAxios.onPost('/bugs').reply(200, saveBug);
 
 		const store = configureStore();
-		const bug = { description: 'a'};
 		await store.dispatch(addbugs(bug));
-		expect(store.getState().entities.bugs.list).toHaveLength(1);
+		expect(store.getState().entities.bugs.list).toContainEqual(saveBug);
 	});
 
 });
